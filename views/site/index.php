b8369e4bcdbf95d62b22f809873d3e7d2756d2f0
hasan <hassanhitleap1@gmail.com>
2018-07-16 11:14:54 +0300

    alarbia rename app

--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'alarbia';
 ?>
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
