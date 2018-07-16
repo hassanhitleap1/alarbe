b2feb044015aa12912be70da02bc0bc479c85c8f
hasan <hassanhitleap1@gmail.com>
2018-07-16 10:13:02 +0300

    add layouts

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -11,12 +11,14 @@ class Module extends \yii\base\Module
      * @inheritdoc
      */
     public $controllerNamespace = 'app\modules\admin\controllers';
+    public $layouts='@app/modules/admin/views/layouts/main';
 
     /**
      * @inheritdoc
      */
     public function init()
     {
+        $this->layout=$this->layouts;
         parent::init();
 
         // custom initialization code goes here
