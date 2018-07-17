4129753cd4fa4163f8c5ecf7124eff9c19168d4a
hasan <hassanhitleap1@gmail.com>
2018-07-17 13:00:31 +0300

    slider full in file

---
+++ views/layouts/slider.php
@@ -0,0 +1,38 @@
+<div id="carousel-example" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example" data-slide-to="1"></li>
+    <li data-target="#carousel-example" data-slide-to="2"></li>
+  </ol>
+
+  <div class="carousel-inner">
+    <div class="item active">
+      <a href="#"><img src="http://finder2000.lk/wp-content/themes/BLANK-Theme/img/1.jpg" /></a>
+      <div class="carousel-caption">
+        <h3>Smart 1</h3>
+        <p>Lets Begin</p>
+      </div>
+    </div>
+    <div class="item">
+      <a href="#"><img src="http://finder2000.lk/wp-content/themes/BLANK-Theme/img/2.jpg" /></a>
+      <div class="carousel-caption">
+        <h3>Smart 2</h3>
+        <p>Lets Begin</p>
+      </div>
+    </div>
+    <div class="item">
+      <a href="#"><img src="http://finder2000.lk/wp-content/themes/BLANK-Theme/img/3.jpg" /></a>
+      <div class="carousel-caption">
+        <h3>Smart 3</h3>
+        <p>Lets Begin</p>
+      </div>
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
