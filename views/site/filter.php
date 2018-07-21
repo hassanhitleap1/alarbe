8059e0df1366ade402c65f8291abac33e5730bc2
hasan kiwan <email@example.com>
2018-07-21 13:58:03 -0700

    modify box filter

--- views/site/filter.php
+++ views/site/filter.php
@@ -19,99 +19,63 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="panel-heading">Panel Heading</div>
             <div class="panel-body">
                     <div class="row">
-                        <div class="col-md-12">
-                            <form class="form-inline">
-                                <div class="form-group">
-                                        <select id="" class="form-control">
+                        
+                            <div class="form-inline">
+                                <div class="form-group col-md-2">
+                                        <select id="" class="form-control col-md-3 input-lg">
                                             <option selected disabled>---</option>
                                             <?php foreach ($areas as $area) : ?>
                                                 <option><?= $area->name_en ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                 </div>
-                                <div class="form-group">
-                                        <select id="" class="form-control">
+                                <div class="form-group col-md-2">
+                                        <select id="" class="form-control input-lg">
                                             <option selected disabled>---</option>
                                             <?php foreach ($categoris as $category) : ?>
                                                 <option><?= $category->name_en ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                 </div>
-                                <div class="form-group">
-                                        <select id="" class="form-control">
+                                <div class="form-group col-md-2 ">
+                                        <select id="" class="form-control input-lg">
                                             <option selected disabled>---</option>
                                             <?php foreach ($countries as $country) : ?>
                                                 <option><?= $country->name_en ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                 </div>
-                                <?php 
-                                $form = ActiveForm::begin([
-                                ])
-                                ?>
-                                <div class="form-gtoup">
-                                <?php
-                                // $form->field($model, 'category')->widget(Select2::classname(), [
-                                //     //'data' => $data,
-                                //     'language' => 'en',
-                                //     'options' => ['placeholder' => 'Select a state ...'],
-                                //     'pluginOptions' => [
-                                //         'allowClear' => true
-                                //     ],
-                                // ]);
-                                ?>
-                                    <?php
-                                    // Select2::widget([
-                                    //     'model' => $model,
-                                    //     'attribute' => 'category',
-                                    //       'data' => function($categoris){
-                                    //           $data=[];
-                                    //           foreach ($categoris as $category ) {
-                                    //               $data[]= $category->name_en;
-                                    //           }
-                                    //           return $data;
-                                    //       },
-                                    //     'options' => ['placeholder' => 'Select a state ...'],
-                                    //     'pluginOptions' => [
-                                    //         'allowClear' => true
-                                    //     ],
-                                    // ]);
-                                     ?>
-                                </div>
-                                <div class="form-group">
-                                        <select id="" class="form-control">
+                                <div class="form-group col-md-2">
+                                        <select id="" class="form-control input-lg " >
                                             <option selected disabled>---</option>
                                             <?php foreach ($subCategories as $subCategory) : ?>
                                                 <option><?= $subCategory->name_en ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                 </div>
-                                
-                                <div class="form-group has-success has-feedback">
-                                    <label class="control-label" for="inputSuccess4"></label>
-                                    <input type="text" class="form-control" id="inputSuccess4">
-                                    <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                                <div class="form-group col-md-2">
+                                    <button type="submit" class="btn btn-primary btn-block btn-lg">find</button>
                                 </div>
-                                <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
-                            </form>
-                        </div>
+                                
+                            </div>
+
                     </div>
-                    <div class="row">
+                    <div class="row clear">
                         <div class="col-md-12">
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 1</label>
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 2</label>
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 3</label>
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 1</label>
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 2</label>
-                            <label class="checkbox-inline"><input type="checkbox" value="">Option 3</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 1</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 2</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 3</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 1</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 2</label>
+                            <label class="checkbox-inline input-lg"><input type="checkbox" value="">Option 3</label>
                         </div>
                     </div>
             </div>
         </div>
     </div>
 
-
-    <div class="row">
+</div>
+<div class="row">
         <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
@@ -143,5 +107,3 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
         </div>
     </div>
-    <?php ActiveForm::end() ?>
-</div>
