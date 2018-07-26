c2c37bc99f32ecf20d46c917177bcb86d9708a51
hasan kiwan <email@example.com>
2018-07-26 11:46:30 -0700

    add text in slider

--- views/layouts/slider.php
+++ views/layouts/slider.php
@@ -14,17 +14,31 @@ use yii\helpers\Html;
   <div class="carousel-inner">
     <div class="item active">
       <?= Html::img('images\slider\1.jpg',['alt'=>'sds']);?>
+       <div class="carousel-caption d-none d-md-block">
+       <div class="panel panel-primary ">
+        <div class="panel-heading">find you akar</div>
+        <div class="panel-body">Panel Content</div>
+      </div>
+        <h3>alsdhokpk store</h3>
+        <p>ks dsodojs lso</p>
+      </div>
      
     </div>
 
     <div class="item">
     <?= Html::img('images\slider\2.jpg',['alt'=>'sds']);?>
-     
+      <div class="carousel-caption d-none d-md-block">
+        <h3>alsdhokpk store</h3>
+        <p>ks dsodojs lso</p>
+      </div>  
     </div>
 
     <div class="item">
     <?= Html::img('images\slider\3.jpg',['alt'=>'sds']);?>
-      
+      <div class="carousel-caption d-none d-md-block">
+        <h3>alsdhokpk store</h3>
+        <p>ks dsodojs lso</p>
+      </div>      
     </div>
   </div>
 
