9553c65bd455842438ed497dd19005d8afaf8eb3
hasan kiwan <email@example.com>
2018-07-29 11:31:12 -0700

    get  langage 90%

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -13,8 +13,8 @@ class BaseController extends Controller
     {
         $cookies = Yii::$app->response->cookies;
 
-        if(isset($_GET['lang'])){
-            $lang= $_GET['lang'];
+        if (isset($_GET['lang'])) {
+            $lang = $_GET['lang'];
             if ($lang == 'ar') {
                 $cookies->add(new \yii\web\Cookie([
                     'name' => 'lang',
@@ -30,10 +30,43 @@ class BaseController extends Controller
                 ]));
                 Yii::$app->language = $cookies->getValue('lang');
             }
+        } elseif (($cookie = $cookies->get('lang')) !== null) {
+            Yii::$app->language = $cookies->getValue('lang');
+        } else {
+            Yii::$app->language = 'ar';
         }
         return parent::beforeAction($action);
     }
 
+    // function init()
+    // {
+    //     parent::init();
+    //     $cookies = Yii::$app->response->cookies;
+
+    //     if (isset($_GET['lang'])) {
+    //         $lang = $_GET['lang'];
+    //         if ($lang == 'ar') {
+    //             $cookies->add(new \yii\web\Cookie([
+    //                 'name' => 'lang',
+    //                 'value' => 'ar',
+    //                 'expire' => time() + 86400 * 365,
+    //             ]));
+    //             Yii::$app->language = $cookies->getValue('lang');
+    //         } else {
+    //             $cookies->add(new \yii\web\Cookie([
+    //                 'name' => 'lang',
+    //                 'value' => 'en',
+    //                 'expire' => time() + 86400 * 365,
+    //             ]));
+    //             Yii::$app->language = $cookies->getValue('lang');
+    //         }
+    //     } elseif (($cookie = $cookies->get('lang')) !== null) {
+    //         Yii::$app->language = $cookies->getValue('lang');
+    //     } else {
+    //         Yii::$app->language = 'ar';
+    //     }
+
+    // }
 
 }
 ?>
