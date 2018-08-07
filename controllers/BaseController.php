40a241b9c3f6d59f95232f33ff28e775ab00b693
hasan kiwan <email@example.com>
2018-08-07 13:09:35 -0700

    langage

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -9,64 +9,27 @@ use yii\filters\VerbFilter;
 
 class BaseController extends Controller
 {
-    public function beforeAction($action)
-    {
-        $cookies = Yii::$app->response->cookies;
 
-        if (isset($_GET['lang'])) {
-            $lang = $_GET['lang'];
-            if ($lang == 'ar') {
-                $cookies->add(new \yii\web\Cookie([
-                    'name' => 'lang',
-                    'value' => 'ar',
-                    'expire' => time() + 86400 * 365,
-                ]));
-                Yii::$app->language = $cookies->getValue('lang');
-            } else {
-                $cookies->add(new \yii\web\Cookie([
-                    'name' => 'lang',
-                    'value' => 'en',
-                    'expire' => time() + 86400 * 365,
-                ]));
-                Yii::$app->language = $cookies->getValue('lang');
-            }
-        } elseif (($cookie = $cookies->get('lang')) !== null) {
+    function init()
+    {
+        parent::init();
+        $cookies = Yii::$app->request->cookies;
+        if (isset($_GET['lang'])) 
+        {
+            Yii::$app->language = $_GET['lang'];
+            $cookie = new Yii\web\cookie([
+                'name' => 'lang',
+                'value' => $_GET['lang']
+            ]);
+            Yii::$app->getResponse()->getCookies()->add($cookie);
+        }
+        else if ($cookies->has('lang')) {
             Yii::$app->language = $cookies->getValue('lang');
         } else {
             Yii::$app->language = 'ar';
         }
-        return parent::beforeAction($action);
+    
     }
-
-    // function init()
-    // {
-    //     parent::init();
-    //     $cookies = Yii::$app->response->cookies;
-
-    //     if (isset($_GET['lang'])) {
-    //         $lang = $_GET['lang'];
-    //         if ($lang == 'ar') {
-    //             $cookies->add(new \yii\web\Cookie([
-    //                 'name' => 'lang',
-    //                 'value' => 'ar',
-    //                 'expire' => time() + 86400 * 365,
-    //             ]));
-    //             Yii::$app->language = $cookies->getValue('lang');
-    //         } else {
-    //             $cookies->add(new \yii\web\Cookie([
-    //                 'name' => 'lang',
-    //                 'value' => 'en',
-    //                 'expire' => time() + 86400 * 365,
-    //             ]));
-    //             Yii::$app->language = $cookies->getValue('lang');
-    //         }
-    //     } elseif (($cookie = $cookies->get('lang')) !== null) {
-    //         Yii::$app->language = $cookies->getValue('lang');
-    //     } else {
-    //         Yii::$app->language = 'ar';
-    //     }
-
-    // }
 
 }
 ?>
